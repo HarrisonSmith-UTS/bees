fbec7408d8c3ca21c1ef00ccf29b42b588f2de33
Jackobi <jackobi.forsyth@gmail.com>
2016-12-16 17:27:37 +1100

    rotation works
    -Fixed issues with rotations and movements conflicting

--- bees/Assets/MainCharacter/Scripts/MovePlayer.cs
+++ bees/Assets/MainCharacter/Scripts/MovePlayer.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
 
-    //Without making the player input smaller, they zip around too fast
-    //This value divides the float value created from player input
-    public int speedDivider = 5;
+    public float speed = 10;
     
-
+    
 	void Start ()
-    {
-		
+    {	
 	}
 	
 	
 	void Update ()
     {
-
-        //If someone accidentally sets the divider to 0, reset it to 1
-        if (speedDivider == 0)
-        {
-            speedDivider = 1;
-        }
 
         //Create objects to hold the values for player input
         /*
@@ -35,19 +26,9 @@ public class MovePlayer : MonoBehaviour {
         implement some simple value shifts
         */
 
-        float xVal = Input.GetAxis("Horizontal") / speedDivider;
-        float zVal = Input.GetAxis("Vertical") / speedDivider;
+        float xVal = Input.GetAxis("Horizontal");
+        float zVal = Input.GetAxis("Vertical");
 
-        if (xVal != 0)
-        {
-            transform.Translate(xVal, 0f, 0f);
-        }
-
-        if (zVal != 0)
-        {
-            transform.Translate(0f, 0f, zVal);
-        }
-
-        float rot1 = Input.GetAxis("rVert");
+            transform.Translate(xVal * Time.deltaTime * speed, 0f, zVal * Time.deltaTime * speed, Space.World);
 	}
 }
--- bees/Assets/MainCharacter/Scripts/Rotation.cs
+++ bees/Assets/MainCharacter/Scripts/Rotation.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour {
 
-    public float rotationalSpeed = 10;
-    float xRotVal;
-    float yRotVal;
-
 	// Use this for initialization
 	void Start ()
     {	
@@ -16,33 +12,14 @@ public class Rotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        /*float xInputVal = Input.GetAxis("Horizontal");
-        float yInputVal = Input.GetAxis("Vertical");
+       
 
-        if (yInputVal != 0)
-        {
-            yRotVal = 90 * yInputVal;
-            print(yRotVal);
-        }
-   
-        if (xInputVal != 0)
-        {
-            //xRotVal = 90 * xInputVal;
-            xRotVal = xInputVal;
-            transform.Rotate(Vector3.up, xRotVal);
-            print(xRotVal);
-        }*/
+        float rx = Input.GetAxis("Right_Horizontal");
+        float ry = Input.GetAxis("Right_Vertical");
 
-        float leftRot = Input.GetAxis("rHor");
-        float rightRot = Input.GetAxis("rVert");
+        float angle = Mathf.Atan2(rx, ry);
 
-        if (leftRot != 0)
-        {
-            transform.Rotate(Vector3.up, leftRot * rotationalSpeed);
-        }
-        if (rightRot != 0)
-        {
-            transform.Rotate(Vector3.up, rightRot * rotationalSpeed);
-        }
+        transform.rotation = Quaternion.EulerAngles(0,angle,0);
+
     }
 }
