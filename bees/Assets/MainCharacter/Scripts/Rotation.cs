02b434c85fbe0b43c4c2416b92b46431c565ccbb
Jackobi <jackobi.forsyth@gmail.com>
2016-12-16 17:22:15 +1100

    rotation and movement works

--- bees/Assets/MainCharacter/Scripts/Rotation.cs
+++ bees/Assets/MainCharacter/Scripts/Rotation.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour {
 
-    public float rotationalSpeed = 10;
-    float xRotVal;
-    float yRotVal;
-
 	// Use this for initialization
 	void Start ()
     {	
@@ -16,54 +12,14 @@ public class Rotation : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        /*float xInputVal = Input.GetAxis("Horizontal");
-        float yInputVal = Input.GetAxis("Vertical");
+       
 
-        if (yInputVal != 0)
-        {
-            yRotVal = 90 * yInputVal;
-            print(yRotVal);
-        }
-   
-        if (xInputVal != 0)
-        {
-            //xRotVal = 90 * xInputVal;
-            xRotVal = xInputVal;
-            transform.Rotate(Vector3.up, xRotVal);
-            print(xRotVal);
-        }*/
+        float rx = Input.GetAxis("Right_Horizontal");
+        float ry = Input.GetAxis("Right_Vertical");
 
-        /*
-        float leftRot = Input.GetAxis("rHor");
-        float rightRot = Input.GetAxis("rVert");
+        float angle = Mathf.Atan2(rx, ry);
 
-        if (leftRot != 0)
-        {
-            transform.Rotate(Vector3.up, leftRot * rotationalSpeed);
-        }
-        if (rightRot != 0)
-        {
-            transform.Rotate(Vector3.up, rightRot * rotationalSpeed);
-        }
-        */
-
-
-        float leftRot = Input.GetAxis("rHor") * -1;
-        float rightRot = Input.GetAxis("rVert") * -1;
-
-        //print(leftRot);
-        //print(rightRot);
-
-        if (leftRot != 0
-            &&
-            rightRot != 0)
-
-        {
-            float radians = Mathf.Atan2(leftRot, rightRot);
-            float rotate = radians * (180 / Mathf.PI);
-            //print(radians);
-            transform.rotation = Quaternion.Euler(0, rotate, 0);
-        }
+        transform.rotation = Quaternion.EulerAngles(0,angle,0);
 
     }
 }
