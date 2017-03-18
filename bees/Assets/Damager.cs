8e32d7f8a34dc68fd0d452ed95fbe91387caa0bf
Harrison Smith <harrison.pj.smith@gmail.com>
2017-03-18 19:05:34 +1100

    Added basic health management
    - Added damager script
    - Added HealthManager script

---
+++ bees/Assets/Damager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    public float damageAmount;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    /// <summary>
+    /// Should deal damage to whatever it collided with
+    /// </summary>
+    /// <param name="collision"></param>
+    void OnCollisionEnter(Collision collision)
+    {
+        //How to decide how much damage to deal?
+        //Should be 'hard coded' to the component (e.g. a certain weapon does a certain amount of damage each hit). 
+        //damageAmount may be modified by another component if e.g. a damage boost thing happens
+		collision.gameObject.SendMessage("TakeDamage",damageAmount);
+    }
+}
---
+++ bees/Assets/HealthManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public float health;
+    public float defenceModifier = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //Update is called on a fixed basis
+    void FixedUpdate()
+    {
+        if (health <= 0f)
+        {
+            //Death occurs, calls the 'Die' method on this object
+            SendMessage("Die");
+        }
+    }
+
+    /// <summary>
+    /// Call this when taking damage
+    /// </summary>
+    void TakeDamage()
+    {
+        //Decrements health by a default amount of damage (1f)
+        TakeDamage(1f);
+    }
+
+    /// <summary>
+    /// Call this when taking damage and supply the amount of damage dealt
+    /// </summary>
+    void TakeDamage(float damage)
+    {
+        //Decrements health by however much damage
+        health -= damage * defenceModifier;
+    }
+}
