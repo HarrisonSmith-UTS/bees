c555f56034c1cfb7cdb96b49f6e5cb4aaf06d12d
Jackobi <jackobi.forsyth@gmail.com>
2017-03-18 16:59:11 +1100

    Started Spawn Script
    -Added BeeSpawn script

---
+++ bees/Assets/Bee/Scripts/BeeSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeeSpawn: MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+    float timer = 0;
+    float spawnIterator = 0;
+    public float spawnRate;
+
+    public object beePrefab;
+    bool disabled;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if(timer >= 30)
+        {
+            timer = 0;
+            spawnIterator += 1;
+        }
+
+        spawnRate = 5 + (2 * spawnIterator);
+
+
+    }
+}
